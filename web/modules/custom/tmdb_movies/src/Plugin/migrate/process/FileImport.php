620baa401bc3447c42c7f09199865a2631cfac11
fabricio.rocha <fabricio.rocha@boost-it.pt>
2024-09-13 14:53:04 -0300

    wip in migration of movies from tmdb

--- web/modules/custom/tmdb_movies/src/Plugin/migrate/process/FileImport.php
+++ web/modules/custom/tmdb_movies/src/Plugin/migrate/process/FileImport.php
@@ -1,17 +1,21 @@
 <?php
 
-namespace Plugin\migrate\process;
+namespace Drupal\tmdb_movies\Plugin\migrate\process;
 
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Plugin\MigrateProcessInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
+use Drupal\file\Entity\File;
+use GuzzleHttp\Exception\RequestException;
+use Drupal\Core\File\FileSystemInterface;
 
 /**
  * Downloads and saves an image file.
  *
  * @MigrateProcessPlugin(
- *   id = "file_import"
+ *   id = "file_import",
+ *   handle_multiples = TRUE
  * )
  */
 class FileImport extends ProcessPluginBase implements MigrateProcessInterface {
@@ -21,8 +25,35 @@ class FileImport extends ProcessPluginBase implements MigrateProcessInterface {
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     $url = 'https://image.tmdb.org/t/p/w500' . $value;
-    $file_data = file_get_contents($url);
-    $file = file_save_data($file_data, 'public://movies/' . basename($url), FILE_EXISTS_REPLACE);
-    return $file ? $file->id() : NULL;
+    $destination = 'public://movies/' . basename($url);
+
+    try {
+      // Making the HTTP request to obtain the contents of the file.
+      $client = \Drupal::httpClient();
+      $response = $client->get($url);
+      $file_data = $response->getBody()->getContents();
+
+      // Get the public file directory.
+      $file_system = \Drupal::service('file_system');
+      $destination = 'public://movies/' . basename($url);
+
+      // Check if directory exists, if not, create it.
+      if (!$file_system->prepareDirectory('public://movies', FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS)) {
+        throw new \Exception('Unable to prepare directory public://movies.');
+      }
+
+      // Save the file.
+      $file = file_save_data($file_data, $destination, FileSystemInterface::EXISTS_REPLACE);
+
+      // Returning the ID of the saved file.
+      return $file instanceof File ? $file->id() : NULL;
+    }
+    catch (RequestException $e) {
+      \Drupal::logger('tmdb_movies')->error('Error downloading file from @url: @message', [
+        '@url' => $url,
+        '@message' => $e->getMessage(),
+      ]);
+      return NULL;
+    }
   }
 }
---
+++ web/modules/custom/tmdb_movies/src/Plugin/migrate_plus/data_fetcher/HttpTmdb.php
@@ -0,0 +1,157 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\tmdb_movies\Plugin\migrate_plus\data_fetcher;
+
+use Drupal\Component\Utility\NestedArray;
+use GuzzleHttp\Client;
+use Drupal\migrate_plus\AuthenticationPluginInterface;
+use Psr\Http\Message\ResponseInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\migrate\MigrateException;
+use Drupal\migrate_plus\DataFetcherPluginBase;
+use GuzzleHttp\Exception\RequestException;
+
+/**
+ * Retrieve data over an HTTP connection for migration.
+ *
+ * Example:
+ *
+ * @code
+ * source:
+ *   plugin: url
+ *   data_fetcher_plugin: http
+ *   headers:
+ *     Accept: application/json
+ *     User-Agent: Internet Explorer 6
+ *     Authorization-Key: secret
+ *     Arbitrary-Header: foobarbaz
+ *   # Guzzle request options can be added.
+ *   # See https://docs.guzzlephp.org/en/stable/request-options.html
+ *   request_options:
+ *     timeout: 300
+ *     allow_redirects: false
+ * @endcode
+ *
+ * @DataFetcher(
+ *   id = "http_tmdb",
+ *   title = @Translation("HTTP")
+ * )
+ */
+class HttpTmdb extends DataFetcherPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The HTTP client.
+   */
+  protected ?Client $httpClient;
+
+  /**
+   * The request headers.
+   */
+  protected array $headers = [];
+
+  /**
+   * The TMDB API Key.
+   */
+  protected string $apiKey;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->httpClient = \Drupal::httpClient();
+
+    // Get the key API from the key module.
+    $key = \Drupal::service('key.repository')->getKey('tmdb');
+    $this->apiKey = $key->getKeyValue();
+
+    // Ensure there is a 'headers' key in the configuration.
+    $configuration += ['headers' => []];
+    $this->setRequestHeaders($configuration['headers']);
+    // Set GET request-method by default.
+    $configuration += ['method' => 'GET'];
+    $this->configuration['method'] = $configuration['method'];
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setRequestHeaders(array $headers): void {
+    // Set the Authorization header with the token.
+    $this->headers = $headers;
+    $this->headers['Authorization'] = 'Bearer ' . $this->apiKey;
+    $this->headers['Accept'] = 'application/json';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRequestHeaders(): array {
+    return !empty($this->headers) ? $this->headers : [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getResponse($url): ResponseInterface {
+    try {
+      $options = ['headers' => $this->getRequestHeaders()];
+      if (!empty($this->configuration['request_options'])) {
+        $options = NestedArray::mergeDeep($options, $this->configuration['request_options']);
+      }
+      $method = $this->configuration['method'] ?? 'GET';
+      $response = $this->httpClient->request($method, $url, $options);
+      if (empty($response)) {
+        throw new MigrateException('No response at ' . $url . '.');
+      }
+    }
+    catch (RequestException $e) {
+      throw new MigrateException('Error message: ' . $e->getMessage() . ' at ' . $url . '.');
+    }
+    return $response;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getResponseContent(string $url): string {
+    return (string) $this->getResponse($url)->getBody();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getNextUrls(string $url): array {
+    $next_urls = [];
+
+    $headers = $this->getResponse($url)->getHeader('Link');
+    if (!empty($headers)) {
+      $headers = explode(',', $headers[0]);
+      foreach ($headers as $header) {
+        $matches = [];
+        preg_match('/^<(.*)>; rel="next"$/', trim($header), $matches);
+        if (!empty($matches) && !empty($matches[1])) {
+          $next_urls[] = $matches[1];
+        }
+      }
+    }
+
+    return array_merge(parent::getNextUrls($url), $next_urls);
+  }
+
+  /**
+   * Returns the initialized authentication plugin.
+   *
+   *   The authentication plugin.
+   */
+//  public function getAuthenticationPlugin(): AuthenticationPluginInterface {
+//    if (!isset($this->authenticationPlugin)) {
+//      $this->authenticationPlugin = \Drupal::service('plugin.manager.migrate_plus.authentication')->createInstance($this->configuration['authentication']['plugin'], $this->configuration['authentication']);
+//    }
+//    return $this->authenticationPlugin;
+//  }
+
+}
